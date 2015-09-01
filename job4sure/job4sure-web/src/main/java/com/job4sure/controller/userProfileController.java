a1c4297fe295bfce1a8ef583dd036989ca059f26
Arun <arun.s.rathore@gmail.com>
2015-09-01 20:18:40 +0530

    commited by sandeep p

--- job4sure/job4sure-web/src/main/java/com/job4sure/controller/userProfileController.java
+++ job4sure/job4sure-web/src/main/java/com/job4sure/controller/userProfileController.java
@@ -1,18 +1,36 @@
 package com.job4sure.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.job4sure.model.Registration;
 @Controller
 public class userProfileController {
 	
 	@RequestMapping(value = "/userProfile", method = RequestMethod.GET)
-	public String showuserProfile(HttpServletRequest request) {
-		//String loggedInUser = request.getu
+	public String showuserProfile(Map<String, Object> map,ModelMap model, HttpServletRequest request) {
+		HttpSession session=request.getSession();
+		Registration registration = (Registration) session.getAttribute("registration");
+	//	map.put("Registration", new Registration());
+		model.addAttribute("registration", registration);
 		return "userProfilePage";
+	}
+	@RequestMapping(value = "/updateBasicProfile", method = RequestMethod.GET)
+	public String updateBasicProfile(@ModelAttribute("Registration") Registration registration,ModelMap model,HttpServletRequest request) {
+		HttpSession session=request.getSession();
+		 registration = (Registration) session.getAttribute("registration");
+		// map.put("Registration", registration);
+		 model.addAttribute("registration", registration);
+		 return "registration";
 	}
 
 }
--- job4sure/job4sure-web/src/main/webapp/WEB-INF/jsp/userProfile.jsp
+++ job4sure/job4sure-web/src/main/webapp/WEB-INF/jsp/userProfile.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +8,31 @@
 <title>Insert title here</title>
 </head>
 <body>
-user page
+
+  <center>
+   <table border="1">
+    <tr>
+      
+     <th>FirstName</th>
+     <th>Email</th>
+     <th>Contact No</th>
+     <th>update</th>
+     <!-- <th>delete</th>
+    <th>edit</th> -->
+    </tr>
+   
+     <tr>
+      <%-- <td>${contact.id}</td> --%>
+      <td>${registration.fullName}</td>
+       <td>${registration.email}</td>
+        <td>${registration.mobileNo}</td>
+        <td><a href='updateBasicProfile?registrationId=${registration.registrationId}'
+       onclick="return confirm('Are you sure you want to update this item?');">update</a></td>
+     </tr>
+
+   
+   </table>
+  </center>
+
 </body>
 </html>
