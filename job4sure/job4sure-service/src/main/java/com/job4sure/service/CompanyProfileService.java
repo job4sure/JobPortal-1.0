603d47a42fbf1e68c260a9b0850e41380b7a017b
Vipulraj Chauhan <vipulrajsingh.chauhan@aartek.in>
2015-09-07 13:43:44 +0530

    commit company service

--- job4sure/job4sure-service/src/main/java/com/job4sure/service/CompanyProfileService.java
+++ job4sure/job4sure-service/src/main/java/com/job4sure/service/CompanyProfileService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.job4sure.model.CompanyProfileModel;
 
-public interface CompanyProfileService {
+public interface CompanyProfileService{
 	public boolean savecompany_profile(CompanyProfileModel companyProfile) ;
 	public List<CompanyProfileModel>view_profile(CompanyProfileModel companyProfile);
 	public CompanyProfileModel getLoggedInCompanyCompleteInfo(
