c080c090645224583437b126e7fdd2aeb85fae26
mayank shukla <mayank.shukla@aartek.co.in>
2015-09-09 15:21:21 +0530

    Add two columns in job_description table and create one salary table

---
+++ job4sure/job4sure-data/src/main/resources/script/salary.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.14 
+MySQL - 5.7.6-m16-log : Database - jobportal
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`jobportal` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `jobportal`;
+
+/*Table structure for table `skills` */
+
+DROP TABLE IF EXISTS `skills`;
+
+CREATE TABLE `skills` (
+  `SKILLS_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `SKILLS_NAME` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`SKILLS_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `skills` */
+
+insert  into `skills`(`SKILLS_ID`,`SKILLS_NAME`) values (1,'c'),(2,'c++'),(3,'java');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
