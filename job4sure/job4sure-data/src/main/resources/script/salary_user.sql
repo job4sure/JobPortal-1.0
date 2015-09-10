95d51298c50658161581ed2918ec24e7622514bc
Sandeep patidar <sandeep.patidar@aartek.in>
2015-09-10 16:25:29 +0530

    commit script for userprofile and salary_user

---
+++ job4sure/job4sure-data/src/main/resources/script/salary_user.sql
@@ -0,0 +1,34 @@
+/*
+SQLyog - Free MySQL GUI v5.17
+Host - 5.0.24-community-nt : Database - jobportal
+*********************************************************************
+Server version : 5.0.24-community-nt
+*/
+
+SET NAMES utf8;
+
+SET SQL_MODE='';
+
+create database if not exists `jobportal`;
+
+USE `jobportal`;
+
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+
+/*Table structure for table `salary_user` */
+
+DROP TABLE IF EXISTS `salary_user`;
+
+CREATE TABLE `salary_user` (
+  `ID` int(11) NOT NULL auto_increment,
+  `SALARY` varchar(30) default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `salary_user` */
+
+insert into `salary_user` (`ID`,`SALARY`) values (1,'10000'),(2,'50000'),(3,'100000'),(4,'500000'),(5,'1000000'),(6,'6000000'),(7,'7000000'),(8,'8000000');
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
---
+++ job4sure/job4sure-data/src/main/resources/script/userprofile.sql
@@ -0,0 +1,51 @@
+/*
+SQLyog - Free MySQL GUI v5.17
+Host - 5.0.24-community-nt : Database - jobportal
+*********************************************************************
+Server version : 5.0.24-community-nt
+*/
+
+SET NAMES utf8;
+
+SET SQL_MODE='';
+
+create database if not exists `jobportal`;
+
+USE `jobportal`;
+
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
+
+/*Table structure for table `userprofile` */
+
+DROP TABLE IF EXISTS `userprofile`;
+
+CREATE TABLE `userprofile` (
+  `USERID` int(11) NOT NULL auto_increment,
+  `INDUSTRY` varchar(255) default NULL,
+  `STATE` varchar(255) default NULL,
+  `ADDRESS` varchar(255) default NULL,
+  `CITY` varchar(255) default NULL,
+  `CURRENT_LOCATION` varchar(255) default NULL,
+  `PREFFERED_LOCATION` varchar(255) default NULL,
+  `REGISTRATION_ID` int(11) default NULL,
+  `RESUME_TITLE` varchar(255) default NULL,
+  `ROLE` varchar(255) default NULL,
+  `SALARY` varchar(255) default NULL,
+  `ZIP` varchar(255) default NULL,
+  `DATE_OF_BIRTH` varchar(255) default NULL,
+  `GENDER` varchar(255) default NULL,
+  `EXPERIENCE` varchar(255) default NULL,
+  `MARITAL_STATUS` varchar(255) default NULL,
+  `PHONE` varchar(255) default NULL,
+  PRIMARY KEY  (`USERID`),
+  KEY `FK_j3l0eat83i2g73fu3y2f27fki` (`REGISTRATION_ID`),
+  CONSTRAINT `FK_j3l0eat83i2g73fu3y2f27fki` FOREIGN KEY (`REGISTRATION_ID`) REFERENCES `login` (`REGISTRATION_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `userprofile` */
+
+insert into `userprofile` (`USERID`,`INDUSTRY`,`STATE`,`ADDRESS`,`CITY`,`CURRENT_LOCATION`,`PREFFERED_LOCATION`,`REGISTRATION_ID`,`RESUME_TITLE`,`ROLE`,`SALARY`,`ZIP`,`DATE_OF_BIRTH`,`GENDER`,`EXPERIENCE`,`MARITAL_STATUS`,`PHONE`) values (1,NULL,'ghfghf','indorewala','jj','indore','delhi',1,'deepak','managere','100000','cghfgh','09/09/2015','Male','12','single','8982198449');
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
