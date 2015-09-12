56df59f7a753df49f63dcffa50ff52ed9a50f0b0
mayank shukla <mayank.shukla@aartek.co.in>
2015-09-12 18:53:08 +0530

    Apply naming convention and reduce code of line

--- job4sure/job4sure-web/src/main/java/com/job4sure/controller/RegistrationController.java
+++ job4sure/job4sure-web/src/main/java/com/job4sure/controller/RegistrationController.java
@@ -21,51 +21,48 @@ import com.job4sure.validator.RegistrationValidator;
 @Controller
 public class RegistrationController {
 
-	@Autowired
-	private RegistrationService registrationService;
+    @Autowired
+    private RegistrationService registrationService;
 
-	@Autowired
-	private RegistrationValidator registrationValidator;
+    @Autowired
+    private RegistrationValidator registrationValidator;
 
-	@RequestMapping("/registration")
-	public String showRegistration(Map<String, Object> map) {
-		map.put("Registration", new Registration());
-		return "registration";
-	}
+    @RequestMapping("/registration")
+    public String showRegistration(Map<String, Object> map) {
+	map.put("Registration", new Registration());
+	return "registration";
+    }
 
-	@RequestMapping(value = "/saveRegistration", method = { RequestMethod.GET, RequestMethod.POST })
-	private String saveUserInformation(@ModelAttribute("Registration") Registration registration, ModelMap model,
-			BindingResult result) throws Exception {
-		boolean status = false;
-		registrationValidator.validate(registration, result);
-		if (result.hasErrors()) {
-			return "registration";
-		}
-		status = registrationService.saveUserInformation(registration);
-		if (status) {
-			model.addAttribute("message", IConstant.REGISTRATION_SUCCESS_MESSAGE);
-		} else {
-			model.addAttribute("message", IConstant.REGISTRATION_FAILURE_MESSAGE);
-		}
-		return "redirect:/OpenloginPage";
-	}
+    @RequestMapping(value = "/saveRegistration", method = { RequestMethod.GET, RequestMethod.POST })
+    private String saveUserInformation(@ModelAttribute("Registration") Registration registration, ModelMap model,
+		    BindingResult result) throws Exception {
+	boolean status = false;
+	registrationValidator.validate(registration, result);
+	if (result.hasErrors())
+	    return "registration";
+	status = registrationService.saveUserInformation(registration);
+	if (status)
+	    model.addAttribute("message", IConstant.REGISTRATION_SUCCESS_MESSAGE);
+	else
+	    model.addAttribute("message", IConstant.REGISTRATION_FAILURE_MESSAGE);
+	return "redirect:/OpenloginPage";
+    }
 
-	@RequestMapping(value = "/verify", method = { RequestMethod.GET, RequestMethod.POST })
-	public String verifyUser(@RequestParam(required = false) String registrationId) throws Exception {
-		String decryptedId = EncryptDecrypt.decrypt(registrationId);
-		Integer regId = Integer.parseInt(decryptedId);
-		registrationService.verifyUser(regId);
-		return "redirect:/OpenloginPage";
-	}
+    @RequestMapping(value = "/verify", method = { RequestMethod.GET, RequestMethod.POST })
+    public String verifyUser(@RequestParam(required = false) String registrationId) throws Exception {
+	String decryptedId = EncryptDecrypt.decrypt(registrationId);
+	Integer regId = Integer.parseInt(decryptedId);
+	registrationService.verifyUser(regId);
+	return "redirect:/OpenloginPage";
+    }
 
-	@RequestMapping(value = "/verifyUserEmailId", method = { RequestMethod.GET })
-	@ResponseBody
-	public String verifyUserEmailId(@RequestParam(required = false) String emailId) {
-		boolean status = false;
-		status = registrationService.verifyUserEmailId(emailId);
-		if (status)
-			return "true";
-		else
-			return "false";
-	}
+    @RequestMapping(value = "/verifyUserEmailId", method = { RequestMethod.GET })
+    @ResponseBody
+    public String verifyUserEmailId(@RequestParam(required = false) String emailId) {
+	boolean status = registrationService.verifyUserEmailId(emailId);
+	if (status)
+	    return "true";
+	else
+	    return "false";
+    }
 }
