2f0838c114e0b94be3a272ffbd65a3f9d9d4536c
mayank shukla <mayank.shukla@aartek.co.in>
2015-09-21 15:53:01 +0530

    DW-111 Removed  unused repository

--- job4sure/job4sure-data/src/main/java/com/job4sure/repository/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.job4sure.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.job4sure.model.Location;
-
-public interface LocationRepository extends JpaRepository<Location, Integer> {
-
-}
--- job4sure/job4sure-data/src/main/java/com/job4sure/repository/PrefferedLocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.job4sure.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.job4sure.model.PrefferedLocation;
-
-public interface PrefferedLocationRepository extends JpaRepository<PrefferedLocation, Integer> {
-
-}
